3b74629c0923a02769d214dbc35853b69ae17a69
Racso1624 <fer10_lb@outlook.com>
2021-09-30 14:50:23 -0600

    Se agregan operaciones graficas

--- simulador_lissajous.py
+++ simulador_lissajous.py
@@ -1,3 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from time import time
+
 """
 @author: Pedro Pablo Arriola Jimenez (20188)
 @author: Oscar Fernando López Barrios (20679)
@@ -22,3 +27,40 @@ entre otros.
 
 """
 
+distancia = 0.03
+carga = 1.602e-17
+masa = 1.67e-27
+velocidad = 5e+5
+voltaje = 1
+
+fig, ax = plt.subplots()
+datosx, datosy = [], []
+datos, = plt.plot([], [], 'ro')
+
+def init():
+    ax.set_xlim(0, 0.4)
+    ax.set_ylim(-0.1, 0.1)
+    return datos,
+
+def update(frame):
+    datosx.append(frame)
+    datosy.append(((carga * voltaje)/(2 * masa * distancia)) * (( frame /velocidad) ** 2))
+    datos.set_data(datosx, datosy)
+    return datos,
+
+def funcioncontador():
+    #Ciclo para tomar el tiempo
+    lista = {}
+    contador = 0.001
+    for i in range(300):
+        ejey = ((carga * voltaje)/(2 * masa * distancia)) * (( contador /velocidad) ** 2)
+        lista[contador] = ejey
+        contador =+ 0.001
+
+tiempoinicio = time()
+funcioncontador()
+tiempo = time() - tiempoinicio
+
+ani = FuncAnimation(fig, update, frames=np.linspace(0.001, 0.3, 20), init_func=init, blit=True)
+plt.text(0.08, 0.08, "Tiempo de ejecucion: " + str(tiempo), fontsize=10, color='green')
+plt.show()
