61982fc0e8b7fe56988f5d14e956d0e7851d327f
unclepete-20 <pedroparty310@gmail.com>
2021-09-30 14:27:07 -0600

    Primer commit con el encabezado del proyecto

---
+++ simulador_lissajous.py
@@ -0,0 +1,24 @@
+"""
+@author: Pedro Pablo Arriola Jimenez (20188)
+@author: Oscar Fernando López Barrios (20679)
+
+@filename: simulador_lissajous.py
+
+Fecha: 30/09/2021
+Curso: Física 3 (Laboratorio)
+Sección: 10
+Catedrático: Luis Mijangos
+
+Descrición del proyecto:
+
+Este proyecto del laboratorio de Física 3 consiste en diseñar
+y simular el comportamiento de un tubo de rayos catódicos que
+posteriormente mostrarán unas formas llamadas "Lissajous" en
+pantalla por medio de las modificaciones que realice el usuario
+al momento de ejecutar el programa. En este proyecto se aplicarán
+todos los conocimientos que hasta el momento se ha visto en el curso
+como por ejemplo el potencial eléctrico, campo eléctrico, capacitancia,
+entre otros.
+
+"""
+
