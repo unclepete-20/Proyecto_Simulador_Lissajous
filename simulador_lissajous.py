b98e6d73e03ca5e300506ae46209ac50b4f6dcc2
Racso1624 <fer10_lb@outlook.com>
2021-09-30 15:20:03 -0600

    Se realiza la grafica principal

--- simulador_lissajous.py
+++ simulador_lissajous.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-from time import time
 
 """
 @author: Pedro Pablo Arriola Jimenez (20188)
@@ -27,40 +26,25 @@ entre otros.
 
 """
 
-distancia = 0.03
-carga = 1.602e-17
-masa = 1.67e-27
-velocidad = 5e+5
-voltaje = 1
+a = 1
+b = 2
+delta = 3.14/2
 
 fig, ax = plt.subplots()
 datosx, datosy = [], []
 datos, = plt.plot([], [], 'ro')
 
 def init():
-    ax.set_xlim(0, 0.4)
-    ax.set_ylim(-0.1, 0.1)
+    ax.set_xlim(-5, 5)
+    ax.set_ylim(-5, 5)
     return datos,
 
 def update(frame):
-    datosx.append(frame)
-    datosy.append(((carga * voltaje)/(2 * masa * distancia)) * (( frame /velocidad) ** 2))
+    datosx.append(np.sin((a*frame) + delta))
+    datosy.append(np.sin(b*frame))
     datos.set_data(datosx, datosy)
     return datos,
 
-def funcioncontador():
-    #Ciclo para tomar el tiempo
-    lista = {}
-    contador = 0.001
-    for i in range(300):
-        ejey = ((carga * voltaje)/(2 * masa * distancia)) * (( contador /velocidad) ** 2)
-        lista[contador] = ejey
-        contador =+ 0.001
-
-tiempoinicio = time()
-funcioncontador()
-tiempo = time() - tiempoinicio
-
-ani = FuncAnimation(fig, update, frames=np.linspace(0.001, 0.3, 20), init_func=init, blit=True)
-plt.text(0.08, 0.08, "Tiempo de ejecucion: " + str(tiempo), fontsize=10, color='green')
+ani = FuncAnimation(fig, update, frames=np.linspace(2, 20, 100), init_func=init, blit=True)
+#plt.text(0.08, 0.08, "Tiempo de ejecucion: " + str("HOLA"), fontsize=10, color='green')
 plt.show()
