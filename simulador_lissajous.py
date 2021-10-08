88e05b2a4456d8436ee6e949d8ea3a415194492c
Racso1624 <fer10_lb@outlook.com>
2021-10-07 19:07:52 -0600

    Se sigue con la interfaz
    Co-authored-by: unclepete-20 <unclepete-20@users.noreply.github.com>

--- simulador_lissajous.py
+++ simulador_lissajous.py
@@ -35,17 +35,26 @@ from tkinter import Tk, Frame, Button, Label, ttk, font
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from functools import partial
 
+def lissajous_delta(value):
 
-"""ventana = tk.Tk()
-ventana.geometry('700x700')
-ventana.wm_title('Simulacion del Comportamiento de un Tubo de Rayos Catodicos')
-ventana.minsize(width=700,height=700)
+    global delta
 
-frame = tk.Frame(ventana, bg='white', bd=3)
-frame.pack(expand=1, fill='both')"""
+    if(value == 1):
+        delta = 0*(math.pi)
+    elif(value == 2):
+        delta = (1/4)*(math.pi)
+    elif(value == 3):
+        delta = (1/2)*(math.pi)
+    elif(value == 4):
+        delta = (3/4)*(math.pi)
+    elif(value == 5):
+        delta = math.pi
 
-def grafico(Vaceleracion, Vvertical, Vhorizontal): #Función que dibuja las Lissajous.
+    print(delta)
 
+    return delta
+
+def calculos(Vaceleracion, Vvertical, Vhorizontal):
     #Variables que modifica el usuario.
     vy = Vvertical #Voltaje vertical (Se modifica)
     vx = Vhorizontal #Voltaje horizontal (Se modifica)
@@ -69,12 +78,17 @@ def grafico(Vaceleracion, Vvertical, Vhorizontal): #Función que dibuja las Liss
     angulo = math.degrees(math.atan(distanciay / ladoplaca))
     angulo2 = math.degrees(math.atan(distanciay / ladoplaca))
 
+    return distanciax, distanciay
+
+
+def grafico(distanciax, distanciay): #Función que dibuja las Lissajous.
 
     fig, ax = plt.subplots()
     plt.title("Tubo de Rayos Catolicos", color='black', size=16,family="Arial")
     x = distanciax
     y = distanciay
     datos, = plt.plot([], [], 'ro')
+    fig.add_subplot(221)
 
     print(x, y)
 
@@ -87,31 +101,16 @@ def grafico(Vaceleracion, Vvertical, Vhorizontal): #Función que dibuja las Liss
         datos.set_data(x, y)
         return datos,
 
-    #ani = FuncAnimation(fig, update, frames=np.linspace(2, 20, 100), init_func=init, blit=True)
-    #plt.text(0.08, 0.08, "Tiempo de ejecucion: " + str("HOLA"), fontsize=10, color='green')
+    ani1 = FuncAnimation(fig, update, frames=np.linspace(2, 20, 100), init_func=init, blit=True)
 
     #Parte de Lissojous
     a = 1
     b = 1
 
-    def lissajous_delta(value):
-        global delta
-
-        if(value == 1):
-            delta = 0*(math.pi)
-        elif(value == 2):
-            delta = (1/4)*(math.pi)
-        elif(value == 3):
-            delta = (1/2)*(math.pi)
-        elif(value == 4):
-            delta = (3/4)*(math.pi)
-        elif(value == 5):
-            delta = math.pi
-
-
     fig, ax = plt.subplots()
     datosx, datosy = [], []
     datos, = plt.plot([], [], 'ro')
+    fig.add_subplot(222)
 
     def init():
         ax.set_xlim(-0.25, 0.25)
@@ -124,121 +123,119 @@ def grafico(Vaceleracion, Vvertical, Vhorizontal): #Función que dibuja las Liss
         datos.set_data(datosx, datosy)
         return datos,
 
-    def iniciar(canvas):
-        ani = FuncAnimation(fig, update, frames=np.linspace(2, 20, 100), init_func=init, blit=True)
-        canvas.draw()
+    ani2 = FuncAnimation(fig, update, frames=np.linspace(2, 20, 100), init_func=init, blit=True)
+
+    return fig, ani1, ani2
+
+def iniciar(canvas, ani1, ani2):
+    animacion1 = ani1
+    animacion2 = ani2
+    canvas.draw()
 
 
+def opennewwindow(ventana):
+    # Toplevel object which will
+    # be treated as a new window
+    graficas = tkinter.Toplevel(ventana)
 
-    ventana = Tk()
-    ventana.geometry('1920x1080')
-    ventana.wm_title('Proyecto Fisica 3 / Pedro Arriola (20188) y Oscar López (20679)')
-    ventana.minsize(width=1920, height=1080)
+    # sets the title of the
+    # Toplevel widget
+    graficas.wm_title('Proyecto Fisica 3')
 
-    def opennewwindow(ventana):
-        # Toplevel object which will
-        # be treated as a new window
-        graficas = tkinter.Toplevel(ventana)
+    # sets the geometry of toplevel
+    graficas.geometry('1920x1080')
+    graficas.minsize(width=1920, height=1080)
 
-        # sets the title of the
-        # Toplevel widget
-        graficas.wm_title('Proyecto Fisica 3')
+    frame = Frame(graficas, bg='white', bd=3)
+    frame.pack(expand=1, fill='both')
 
-        # sets the geometry of toplevel
-        graficas.geometry('1920x1080')
-        graficas.minsize(width=1920, height=1080)
+    canvas = FigureCanvasTkAgg(grafico(), master = frame)
+    canvas.get_tk_widget().pack(padx=5, pady=10, expand=1, fill='both', side=tkinter.RIGHT)
 
-        frame = Frame(graficas, bg='white', bd=3)
-        frame.pack(expand=1, fill='both')
-
-        canvas = FigureCanvasTkAgg(fig, master = frame)
-        canvas.get_tk_widget().pack(padx=5, pady=10, expand=1, fill='both', side=tkinter.RIGHT)
-
-        print(delta)
-
-        iniciar(canvas)
-
-    # Títulos
-
-    static_title = tkinter.Label(master=ventana, text="⌁ PARÁMETROS MODIFICABLES ⌁", font=("Arial", 20), fg="#3892EA")
-    static_title.place(relx=0.50, rely=0.12, relwidth=1.1, relheight=0.125, anchor=tkinter.CENTER)
-
-    # Voltajes
-
-    x_voltage = tkinter.Label(master=ventana, text="Vx", font=("Arial", 14), fg="black")
-    x_voltage.place(relx=0.09375, rely=0.367, anchor=tkinter.CENTER)
-
-    y_voltage = tkinter.Label(master=ventana, text="Vy", font=("Arial", 14), fg="black")
-    y_voltage.place(relx=0.09375, rely=0.52, anchor=tkinter.CENTER)
-
-    # Sliders
-
-    x_slider = tkinter.Scale(master=ventana, from_=-8000, to=8000, orient=tkinter.HORIZONTAL)
-    x_slider.place(relx=0.3, rely=0.4, relwidth=0.25, relheight=0.15, anchor=tkinter.CENTER)
-
-    y_slider = tkinter.Scale(master=ventana, from_=-8000, to=8000, orient=tkinter.HORIZONTAL)
-    y_slider.place(relx=0.3, rely=0.55, relwidth=0.25, relheight=0.15, anchor=tkinter.CENTER)
-
-    # Parámetros
-
-    a_label = tkinter.Label(master=ventana, text="A", font=("Arial", 20), fg="black")
-    a_label.place(relx=0.6, rely=0.35, anchor=tkinter.CENTER)
-
-    b_label = tkinter.Label(master=ventana, text="B", font=("Arial", 20), fg="black")
-    b_label.place(relx=0.75, rely=0.35, anchor=tkinter.CENTER)
-
-    c_label = tkinter.Label(master=ventana, text="Delta\n(δ)", font=("Arial", 20), fg="black")
-    c_label.place(relx=0.9, rely=0.35, anchor=tkinter.CENTER)
-
-    frequency_label = tkinter.Label(master=ventana, text="Frecuencia", font=("Arial", 14), fg="black")
-    frequency_label.place(relx=0.09375, rely=0.675, anchor=tkinter.CENTER)
-
-    interval_label = tkinter.Label(master=ventana, text="Intervalo", font=("Arial", 14), fg="black")
-    interval_label.place(relx=0.09375, rely=0.780, anchor=tkinter.CENTER)
-
-    # Function inputs
-
-    a_input = tkinter.Entry(master=ventana, justify='center')
-    a_input.place(relx=0.6, rely=0.5, relwidth=0.1, anchor=tkinter.CENTER)
-
-    b_input = tkinter.Entry(master=ventana, justify='center')
-    b_input.place(relx=0.75, rely=0.5, relwidth=0.1, anchor=tkinter.CENTER)
-
-    frequency_input = tkinter.Entry(master=ventana, justify='center')
-    frequency_input.place(relx=0.225, rely=0.675, relwidth=0.1, anchor=tkinter.CENTER)
-
-    interval_input = tkinter.Entry(master=ventana, justify='center')
-    interval_input.place(relx=0.225, rely=0.780, relwidth=0.1, anchor=tkinter.CENTER)
-
-    # Botones
-
-    delta_inputone = tkinter.Button(master=ventana, text='0', command=lissajous_delta(1))
-    delta_inputone.place(relx=0.9, rely=0.5, relwidth=0.1, anchor=tkinter.CENTER)
-
-    delta_inputtwo = tkinter.Button(master=ventana, text='π/4', command=lissajous_delta(2))
-    delta_inputtwo.place(relx=0.9, rely=0.60, relwidth=0.1, anchor=tkinter.CENTER)
-
-    delta_inputthree = tkinter.Button(master=ventana, text='π/2', command=lissajous_delta(3))
-    delta_inputthree.place(relx=0.9, rely=0.70, relwidth=0.1, anchor=tkinter.CENTER)
-
-    delta_inputfour = tkinter.Button(master=ventana, text='3π/4', command=lissajous_delta(4))
-    delta_inputfour.place(relx=0.9, rely=0.80, relwidth=0.1, anchor=tkinter.CENTER)
-
-    delta_inputfive = tkinter.Button(master=ventana, text='π', command=lissajous_delta(5))
-    delta_inputfive.place(relx=0.9, rely=0.9, relwidth=0.1, anchor=tkinter.CENTER)
-
-    # define font
-    myFont = font.Font(family='Helvetica', size=5, weight='bold')
-
-    graficar = tkinter.Button(master=ventana, text='⌁ Graficar datos ⌁', bg='#3892EA', command=partial(opennewwindow, ventana))
-    graficar.place(relx=0.5, rely=0.9, relwidth=0.3, relheight=0.1, anchor=tkinter.CENTER)
-
-    graficar['font'] = myFont
+    iniciar(canvas)
 
 
-    ventana.mainloop()
+ventana = Tk()
+ventana.geometry('1920x1080')
+ventana.wm_title('Proyecto Fisica 3 / Pedro Arriola (20188) y Oscar López (20679)')
+ventana.minsize(width=1920, height=1080)
 
-    #plt.text(0.08, 0.08, "Tiempo de ejecucion: " + str("HOLA"), fontsize=10, color='green')
-    #plt.show()\
+# Títulos
 
-grafico(100, 100, 100)
+static_title = tkinter.Label(master=ventana, text="⌁ PARÁMETROS MODIFICABLES ⌁", font=("Arial", 20), fg="#3892EA")
+static_title.place(relx=0.50, rely=0.12, relwidth=1.1, relheight=0.125, anchor=tkinter.CENTER)
+
+# Voltajes
+
+x_voltage = tkinter.Label(master=ventana, text="Vx", font=("Arial", 14), fg="black")
+x_voltage.place(relx=0.09375, rely=0.367, anchor=tkinter.CENTER)
+
+y_voltage = tkinter.Label(master=ventana, text="Vy", font=("Arial", 14), fg="black")
+y_voltage.place(relx=0.09375, rely=0.52, anchor=tkinter.CENTER)
+
+# Sliders
+
+x_slider = tkinter.Scale(master=ventana, from_=-8000, to=8000, orient=tkinter.HORIZONTAL)
+x_slider.place(relx=0.3, rely=0.4, relwidth=0.25, relheight=0.15, anchor=tkinter.CENTER)
+
+y_slider = tkinter.Scale(master=ventana, from_=-8000, to=8000, orient=tkinter.HORIZONTAL)
+y_slider.place(relx=0.3, rely=0.55, relwidth=0.25, relheight=0.15, anchor=tkinter.CENTER)
+
+# Parámetros
+
+a_label = tkinter.Label(master=ventana, text="A", font=("Arial", 20), fg="black")
+a_label.place(relx=0.6, rely=0.35, anchor=tkinter.CENTER)
+
+b_label = tkinter.Label(master=ventana, text="B", font=("Arial", 20), fg="black")
+b_label.place(relx=0.75, rely=0.35, anchor=tkinter.CENTER)
+
+c_label = tkinter.Label(master=ventana, text="Delta\n(δ)", font=("Arial", 20), fg="black")
+c_label.place(relx=0.9, rely=0.35, anchor=tkinter.CENTER)
+
+frequency_label = tkinter.Label(master=ventana, text="Frecuencia", font=("Arial", 14), fg="black")
+frequency_label.place(relx=0.09375, rely=0.675, anchor=tkinter.CENTER)
+
+interval_label = tkinter.Label(master=ventana, text="Intervalo", font=("Arial", 14), fg="black")
+interval_label.place(relx=0.09375, rely=0.780, anchor=tkinter.CENTER)
+
+# Function inputs
+
+a_input = tkinter.Entry(master=ventana, justify='center')
+a_input.place(relx=0.6, rely=0.5, relwidth=0.1, anchor=tkinter.CENTER)
+
+b_input = tkinter.Entry(master=ventana, justify='center')
+b_input.place(relx=0.75, rely=0.5, relwidth=0.1, anchor=tkinter.CENTER)
+
+frequency_input = tkinter.Entry(master=ventana, justify='center')
+frequency_input.place(relx=0.225, rely=0.675, relwidth=0.1, anchor=tkinter.CENTER)
+
+interval_input = tkinter.Entry(master=ventana, justify='center')
+interval_input.place(relx=0.225, rely=0.780, relwidth=0.1, anchor=tkinter.CENTER)
+
+# Botones
+
+delta_inputone = tkinter.Button(master=ventana, text='0', command=lissajous_delta(1))
+delta_inputone.place(relx=0.9, rely=0.5, relwidth=0.1, anchor=tkinter.CENTER)
+
+delta_inputtwo = tkinter.Button(master=ventana, text='π/4', command=lissajous_delta(2))
+delta_inputtwo.place(relx=0.9, rely=0.60, relwidth=0.1, anchor=tkinter.CENTER)
+
+delta_inputthree = tkinter.Button(master=ventana, text='π/2', command=lissajous_delta(3))
+delta_inputthree.place(relx=0.9, rely=0.70, relwidth=0.1, anchor=tkinter.CENTER)
+
+delta_inputfour = tkinter.Button(master=ventana, text='3π/4', command=lissajous_delta(4))
+delta_inputfour.place(relx=0.9, rely=0.80, relwidth=0.1, anchor=tkinter.CENTER)
+
+delta_inputfive = tkinter.Button(master=ventana, text='π', command=lissajous_delta(5))
+delta_inputfive.place(relx=0.9, rely=0.9, relwidth=0.1, anchor=tkinter.CENTER)
+
+# define font
+myFont = font.Font(family='Helvetica', size=5, weight='bold')
+
+graficar = tkinter.Button(master=ventana, text='⌁ Graficar datos ⌁', bg='#3892EA', command=partial(opennewwindow, ventana))
+graficar.place(relx=0.5, rely=0.9, relwidth=0.3, relheight=0.1, anchor=tkinter.CENTER)
+
+graficar['font'] = myFont
+
+
+ventana.mainloop()
